fe68237108a5489f9ba6725fd243036f2f08aa26
rp144nj <rp144nj@gmail.com>
2015-05-28 11:27:05 -0400

    gravatar help and tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
     end
     test "valid signup information" do
       get signup_path
@@ -20,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.FILL_IN
   end
 end
